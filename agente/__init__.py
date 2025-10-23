49689493eaab8b9d979e616e7b5b123fe1f002d1
WilsonNous <107373913+WilsonNous@users.noreply.github.com>
2025-10-23 15:11:58 -0300

    Create __init__.py

---
+++ agente/__init__.py
@@ -0,0 +1,6 @@
+# Agente Netunna - Inicialização do módulo
+# Mantém compatibilidade com o Splitter principal
+# Desenvolvido por Nous / Netunna © 2025
+
+__version__ = "4.0"
+__author__ = "Netunna Software"
